6c3bc063a259b9b6701f1e95b39ddf7d23ff6301
Kostadin Devedzhiev <kostadin.devedzhiev@stonybrook.edu>
2021-03-09 20:03:22 -0500

    Implement is_solution method

--- dfsb.py
+++ dfsb.py
@@ -27,17 +27,27 @@ def construct_graph():
         graph[edge[0]].append(edge[1])
         graph[edge[1]].append(edge[0])
     print(graph)
-    return graph, graph_coloring, M, K
+    return graph, graph_coloring, edges, K
 
 
-def is_solution(graph_coloring):
-    pass
+# Checks if generated graph coloring is a solution.
+# Returns true if it is and false otherwise.
+def is_solution(graph_coloring, edges):
+    for node in graph_coloring:
+        if node is None:
+            return False
+    for edge in edges:
+        if graph_coloring[edge[0]] == graph_coloring[edge[1]]:
+            return False
+    return True
+
 
 def DFS(graph, graph_coloring):
     pass
 
 
 if __name__ == '__main__':
-    graph, graph_coloring, num_edges, num_colors = construct_graph()
+    graph, graph_coloring, edges, num_colors = construct_graph()
+    print(is_solution(graph_coloring, edges))
 
     # DFS(graph, '0')
