2e8094c1ef2a2a273e45b11353fdb01afc43cbda
Kostadin Devedzhiev <kostadin.devedzhiev@stonybrook.edu>
2021-03-15 20:33:47 -0400

    New implementation of dfsb

--- dfsb.py
+++ dfsb.py
@@ -3,6 +3,7 @@ from itertools import groupby
 from visual_graph import visualize
 from copy import deepcopy
 import random
+import time
 
 
 # Constructs an adjacency list representation of a graph from the input
@@ -48,19 +49,37 @@ def is_solution(graph_coloring, edges):
 # gets the colors of neighboring nodes
 def get_neighbor_colors(graph, graph_coloring, node):
     colors = []
-    for neighbor in graph[node]:
+    neighbors = graph[node]
+    for neighbor in neighbors:
         if graph_coloring[neighbor] is not None:
             colors.append(graph_coloring[neighbor])
+
     return colors
+
+
+def sorted_colors(graph, graph_coloring, colors, node):
+    neighbor_colors = dict.fromkeys(colors, 0)
+    neighbors = graph[node]
+
+    for neighbor in neighbors:
+        if graph_coloring[neighbor] is not None:
+            neighbor_colors[graph_coloring[neighbor]] += 1
+
+    sorted_neighbor_colors = sorted(neighbor_colors, key=neighbor_colors.get, reverse=True)
+    return sorted_neighbor_colors
 
 
 # Gives the possible branching of a node
 def get_successors(graph, graph_coloring, node, colors):
     successors = []
-    for neighbor in graph[node]:
+    neighbors = graph[node]
+    neighbors.sort(key=lambda x: len(graph[x]))
+    for neighbor in neighbors:
         if graph_coloring[neighbor] is None:
             neighbor_colors = get_neighbor_colors(graph, graph_coloring, neighbor)
-            for color in colors:
+
+            sorter_colors = sorted_colors(graph, graph_coloring, colors, neighbor)
+            for color in sorter_colors:
                 if color not in neighbor_colors:
                     copy_graph_coloring = deepcopy(graph_coloring)
                     copy_graph_coloring[neighbor] = color
@@ -80,12 +99,44 @@ def DFSB(graph, graph_coloring, node, colors, edges):
     return None
 
 
+def getuncolored_verted(graph, graph_coloring, node):
+    nodes = []
+    for item in graph_coloring.items():
+        if item[1] is None:
+            nodes.append(item[0])
+    nodes.sort(key=lambda x: len(graph[x]))
+    if len(nodes)>0:
+        return nodes[0]
+    else:
+        return False
+
+
+def RB(graph, graph_coloring, node,colors,edges):
+    if is_solution(graph_coloring,edges):
+        return graph_coloring
+    neighbor = getuncolored_verted(graph, graph_coloring, node)
+    if neighbor is False:
+        return False
+    neighbor_colors = get_neighbor_colors(graph, graph_coloring, neighbor)
+    for color in colors:
+        if color not in neighbor_colors:
+            graph_coloring[neighbor] = color
+            result = RB(graph, graph_coloring, neighbor, colors, edges)
+            if result is not False:
+                return result
+            graph_coloring[neighbor] = None
+    return False
+
+
 # DFSB DRIVER
 
 def DFSB_DRIVER(graph, graph_coloring, edges, colors):
+    start = time.time()
     graph_coloring['0'] = '0'
-    colored_graph = DFSB(graph, graph_coloring, '0', colors, edges)
+    colored_graph = RB(graph, graph_coloring, '0', colors,edges)
     print(colored_graph)
+    end = time.time()
+    print("TIME FOR MIN CONFLICT: ", 1000 * (end - start))
     visualize(graph)
 
 
@@ -108,8 +159,13 @@ def get_conflicted(graph, graph_coloring, edges):
     return -1
 
 
-def get_least_conflict(graph, graph_coloring, edges, node):
-    pass
+def get_least_conflict(graph, graph_coloring, edges, node, colors):
+    neighbor_colors = dict.fromkeys(colors, 0)
+    neighbors = graph[node]
+    for neighbor in neighbors:
+        neighbor_colors[graph_coloring[neighbor]] += 1
+
+    return min(neighbor_colors, key=neighbor_colors.get)
 
 
 def min_conflict(graph, graph_coloring, edges, colors, max_steps):
@@ -120,15 +176,18 @@ def min_conflict(graph, graph_coloring, edges, colors, max_steps):
             print('Solution found')
             return graph_coloring
 
-        new_color = get_least_conflict(graph, graph_coloring, edges, node)
+        new_color = get_least_conflict(graph, graph_coloring, edges, node, colors)
         graph_coloring[node] = new_color
 
 
 def min_conflict_DRIVER(graph, graph_coloring, edges, colors):
-    min_conflict(graph, graph_coloring, edges, colors, 10000)
+    start = time.time()
+    print(min_conflict(graph, graph_coloring, edges, colors, 1000000))
+    end = time.time()
+    print("TIME FOR MIN CONFLICT: ", start - end)
 
 
 if __name__ == '__main__':
     graph, graph_coloring, edges, colors = construct_graph()
-    # DFSB_DRIVER(graph, graph_coloring, edges, colors)
-    min_conflict_DRIVER(graph, graph_coloring, edges, colors)
+    DFSB_DRIVER(graph, graph_coloring, edges, colors)
+    # min_conflict_DRIVER(graph, graph_coloring, edges, colors)
